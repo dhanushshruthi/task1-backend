dd9ccaf536af276687721e73de86d16f0a1b389f
Dhanushshruthi S T <132667751+dhanushshruthi@users.noreply.github.com>
2025-11-03 18:00:42 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,15 +1,6 @@
-const express = require('express');
-const dotenv = require('dotenv');
-const connectDB = require('./src/config/db');
-const routes = require('./src/routes/index');
-
-dotenv.config();
-connectDB();
-
-const app = express();
-app.use(express.json()); // to parse JSON input
-
-app.use('/api/v1', routes); // base route
+// server.js
+const app = require('./app'); // import the app we just created
 
 const PORT = process.env.PORT || 5000;
+
 app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
